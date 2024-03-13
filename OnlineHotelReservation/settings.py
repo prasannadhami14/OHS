356c76ddf2eb33dfa2c7a70aa84237d5730a7163
PrashannaDhami <prashannadhami14@gmail.com>
2024-03-13 18:58:14 +0545

    first commit

--- OnlineHotelReservation/settings.py
+++ OnlineHotelReservation/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-9q*x_tjne%y&ck@3utrlplu&$igqi_h65xt+s935y5t*=&1tj^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']]
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -143,6 +143,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static/'),
 ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
